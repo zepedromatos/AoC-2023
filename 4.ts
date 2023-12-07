db30ed53bbfe6600e9eb6fd920676268ef7a254b
Zé Pedro Matos <revolty@gmail.com>
2023-12-07 15:23:14 +0000

    day 4 part 2

--- 4.ts
+++ 4.ts
@@ -2,22 +2,31 @@ import { dayFourInput as input } from "./inputs";
 
 const cardsArray = input.split("\n");
 
+const aux = [0];
+
+const makeCopies = (currentCard: number, range: number, increment: number): void => {
+    for (let i = currentCard; i < (currentCard + range) && (i < cardsArray.length ); i++) {
+        aux[i] = (aux[i] || 0) + increment;
+    }
+};
+
 const scores = cardsArray.map((card) => {
 	const match = card.match(
 		/Card\s+(\d+):\s*(\d+(?:\s+\d+)*)\s*\|\s*(\d+(?:\s+\d+)*)/
 	);
 
-	const [, , winningNumbersStr, myNumbersStr] = match;
+	const [, cardNumber, winningNumbersStr, myNumbersStr] = match; 
+    const currentCardCopies = aux[Number(cardNumber) - 1];
 
 	const winningNumbers = winningNumbersStr.split(/\s+/).filter(Boolean).map(Number);
 	const myNumbers = myNumbersStr.split(/\s+/).filter(Boolean).map(Number);
 
 	const myWinningNumbers = myNumbers.filter(num => winningNumbers.includes(num)).length;
 
-    const score = myWinningNumbers === 0 ? 0 : Math.pow(2, myWinningNumbers - 1);
-    return score;   
+    if (myWinningNumbers) makeCopies(Number(cardNumber), myWinningNumbers, currentCardCopies? currentCardCopies + 1 : 1);
 });
 
-const totalScore = scores.reduce((acc, score) => acc + score, 0);
+const amountOfCards = aux.reduce((acc, copies) => acc + copies, 0) + cardsArray.length;
 
-console.log(totalScore);
+console.log(amountOfCards);
+
