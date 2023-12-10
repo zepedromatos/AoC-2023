1610da9b06a6eb9bc2533a0fcb294079f1eaee71
Zé Pedro Matos <revolty@gmail.com>
2023-12-10 15:23:57 +0000

    day 8 part 2

--- 8.ts
+++ 8.ts
@@ -1,56 +1,112 @@
 import { dayEightInput as input } from "./inputs";
 
-console.time("Day 8 part 1 exec time:");
-const finalDestination = "ZZZ";
-let firstLocation = "AAA";
-
-const createDataStructure = (input: string) => {
-	const lines = input.split("\n");
-
-	const emptyLineIndex = lines.indexOf("");
-
-	const locations = lines.slice(emptyLineIndex + 1).reduce((acc, line) => {
-		const regex = /(\w{3}) = \((\w{3}), (\w{3})\)/g;
-
-		const resultObject: Record<string, { L: string; R: string }> = {};
-
-		let match;
-		while ((match = regex.exec(line)) !== null) {
-			const [, key, L, R] = match;
-			resultObject[key] = { L, R };
-            acc[key] = resultObject[key];
-		}
-		return acc;
-	}, {});
-
-	const resultArray = {
-		instructions: lines.slice(0, emptyLineIndex).join("").split(""),
-		locations,
-	};
-
-	return resultArray;
-};
-
-const dataStructure = createDataStructure(input);
-
-const navigate = (dataStructure) => {
-    let { instructions, locations } = dataStructure;
-
-    let currentLocation = firstLocation;
-    let counter = 0;
-
-    while (currentLocation !== finalDestination) {
-        if(!instructions[counter]) instructions = [...instructions, ...instructions];
-        let side = instructions[counter];
-
-        currentLocation = locations[currentLocation][side];
-
-        counter++;
-        if(currentLocation === finalDestination) break;
-    };
-
-    return counter;
-};
-
-console.log(navigate(dataStructure));
-console.timeEnd("Day 8 part 1 exec time:");
+const minCommonMulti = (arr: number[]) => {
+    const gcd = (a: number, b: number) => {
+      return !b ? a : gcd(b, a % b)
+    }
+  
+    const lcm = (a: number, b: number) => {
+      return (a * b) / gcd(a, b)
+    }
+  
+    const initArr = arr.sort((a, b) => a - b)
+    var multiple = initArr[0]
+    initArr.forEach((n) => {
+      multiple = lcm(multiple, n)
+    })
+  
+    return multiple
+  }
+  
+  const goA = (input: string) => {
+    const instructionSet = {
+      R: 1,
+      L: 0,
+    }
+  
+    const [instructionLine, networkLines] = input.split(/\n\n/)
+  
+    const instructions = instructionLine.split("").map((i) => instructionSet[i])
+    const network = {}
+  
+    const nodeLines = networkLines.split(/\n/)
+    let positionNode: string = "AAA"
+    let destinationNode: string = "ZZZ"
+  
+    nodeLines.forEach((line) => {
+      const node = line.split(/ = /)
+      network[node[0]] = node[1].replace(/\(/g, "").replace(/\)/g, "").split(/, /)
+    })
+  
+    let steps = 0
+    let cycle = [...instructions]
+  
+    while (positionNode !== destinationNode) {
+      if (cycle.length === 0) {
+        cycle = [...instructions]
+      }
+  
+      const direction = cycle.shift()
+  
+      positionNode = network[positionNode][direction]
+  
+      steps++
+    }
+  
+    return steps
+  }
+  
+  const goB = (input: string) => {
+    const instructionSet = {
+      R: 1,
+      L: 0,
+    }
+  
+    const [instructionLine, networkLines] = input.split(/\n\n/)
+    const instructions = instructionLine.split("").map((i) => instructionSet[i])
+    const network = {}
+  
+    const nodeLines = networkLines.split(/\n/)
+  
+    nodeLines.forEach((line) => {
+      const node = line.split(/ = /)
+      network[node[0]] = node[1].replace(/\(/g, "").replace(/\)/g, "").split(/, /)
+    })
+  
+    let positionNodes = Object.keys(network).filter((node) => node[2] === "A")
+  
+    let cycle = [...instructions]
+  
+    const positionNodeMap = positionNodes.map((positionNode) => {
+      let steps = 0
+      while (positionNode[2] !== "Z") {
+        if (cycle.length === 0) {
+          cycle = [...instructions]
+        }
+  
+        const direction = cycle.shift()
+  
+        positionNode = network[positionNode][direction]
+  
+        steps++
+      }
+  
+      return steps
+    })
+  
+    return minCommonMulti(positionNodeMap)
+  }
+  
+  /* Tests */
+  
+  // test()
+  
+  /* Results */
+  
+  console.time("Time")
+  const resultA = goA(input)
+  const resultB = goB(input)
+  console.timeEnd("Time")
+  
+  console.log("Solution to part 1:", resultA)
+  console.log("Solution to part 2:", resultB)
